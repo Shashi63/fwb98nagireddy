67fd329fc55a4fbc1a8d175d54a766969af07ec5
Shashi63 <89168145+Shashi63@users.noreply.github.com>
2021-11-03 03:06:00 -0500

    added files

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dogRouter = require('./routes/dog');
+var addmodsRouter = require('./routes/addmods');
 
 var app = express();
 
@@ -21,8 +23,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/dog', dogRouter);
 // catch 404 and forward to error handler
+app.use('/addmods', addmodsRouter);
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
